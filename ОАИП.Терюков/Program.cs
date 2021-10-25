fc793dbf475d8649da6f9e4b0bae085f586bda2e
BAuLids <teryukovdima04@gmail.com>
2021-10-25 13:05:45 +1000

    Добавьте файлы проекта.

---
+++ ОАИП.Терюков/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ОАИП.Терюков
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bs = true;
+            Console.WriteLine("Ugadai cgislo");
+            int.TryParse(Console.ReadLine(), out int b);
+            Random rnd = new Random();
+            int a = rnd.Next(1, 10);
+            do
+            {
+                if (a < b)
+                {
+                    Console.WriteLine("mnogo");
+                    a = a;
+                    bs = false;
+                    int.TryParse(Console.ReadLine(), out b);
+                }
+                if (a > b)
+                {
+                    Console.WriteLine("malo");
+                    a = a;
+                    bs = false;
+                    int.TryParse(Console.ReadLine(), out b);
+                }
+                if (a == b)
+                {
+                    Console.WriteLine("verno");
+                    break;
+                }
+            } while (!bs);
+            Console.ReadLine();
+        }
+    }
+}
